36726d75f83721ac5207b05c98b75a126dc8c5bb
nicolasizquierdo <nicolasizquierdocontacto@gmail.com>
2022-08-01 13:35:27 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import Template from './templates/Template.js';
-console.log('hola');
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.styl';
+console.log('holaaa');
 
 (async function App() {
   const main = null || document.getElementById('main');
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPLugin = require('terser-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
+    },
+    resolve: {
+        extensions: ['.js'],
+        alias: {
+            '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@templates': path.resolve(__dirname, 'src/templates/'),
+            '@styles': path.resolve(__dirname, 'src/styles/'),
+            '@images': path.resolve(__dirname, 'src/assets/images'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                // test nos permite saber que tipo de extensiones vamos a usar
+                // Se utiliza expresiones regulares en este caso (utilizar todos los archivos que empiecen por "m" o "js")
+                test: /\.m?.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css|\.styl$/i,
+                use:[MiniCssExtractPlugin.loader, 
+                    'css-loader',
+                    'stylus-loader'
+                ],
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource' 
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000, // O LE PASAMOS UN BOOLEANOS TRUE O FALSE
+                        // Habilita o deshabilita la transformación de archivos en base64.
+                        mimetype: 'aplication/font-woff',
+                        // Especifica el tipo MIME con el que se alineará el archivo. 
+                        // Los MIME Types (Multipurpose Internet Mail Extensions)
+                        // son la manera standard de mandar contenido a través de la red.
+                        name: "[name].[ext]",
+                        // EL NOMBRE INICIAL DEL ARCHIVO + SU EXTENSIÓN
+                        // PUEDES AGREGARLE [name]hola.[ext] y el output del archivo seria 
+                        // ubuntu-regularhola.woff
+                        outputPath: './assets/fonts/', 
+                        // EL DIRECTORIO DE SALIDA (SIN COMPLICACIONES)
+                        publicPath: './assets/fonts/',
+                        // EL DIRECTORIO PUBLICO (SIN COMPLICACIONES)
+                        esModule: false 
+                        // AVISAR EXPLICITAMENTE SI ES UN MODULO
+                    }
+                },
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // Inserción de los elementos
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'assets/[name].[contenthash].css'
+        }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    // que recurso (archivo o directorio) deseamos copiar al directorio final
+                    from: path.resolve(__dirname, "src", "assets/images"),
+                    // en que ruta dentro de la carpeta final terminara los recursos
+                    to: "assets/images"
+                }
+            ]
+        }),
+        new Dotenv(),
+        new CleanWebpackPlugin(),
+    ],
+    optimization: {
+        minimize: true,
+        minimizer: [
+            // Optimization for css
+            new CssMinimizerPlugin(),
+            // Optimization for js
+            new TerserPLugin()
+        ]
+    }
+}
